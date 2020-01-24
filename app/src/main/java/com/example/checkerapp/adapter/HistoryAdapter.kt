de60b2112a819536d11294f38edb5fe2c3cf644f
Brady Neeft <bradyneeft@gmail.com>
2020-01-24 23:32:31 +0100

    History page added

---
+++ app/src/main/java/com/example/checkerapp/adapter/HistoryAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.checkerapp.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.checkerapp.R
+import com.example.checkerapp.model.StatusHistory
+import kotlinx.android.synthetic.main.history_item.view.*
+
+class HistoryAdapter(private val statuses: List<StatusHistory>)
+    : RecyclerView.Adapter<HistoryAdapter.ViewHolder>() {
+
+    lateinit var context: Context
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.history_item, parent,
+                false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return statuses.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(statuses[position])
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        fun bind(history: StatusHistory) {
+            itemView.tvStatus.text =  context.getString(R.string.status, history.status)
+            itemView.tvLastCheckedInDate.text = context.getString(R.string.last_checked_in_date,
+                history.lastCheckedInDate)
+            if(history.lastCheckedOutDate != null)
+                //TODO deze statement oplossen.
+            itemView.tvLastCheckedOutDate.text = context.getString(R.string.last_checked_out_date,
+                history.lastCheckedOutDate)
+            else
+                itemView.tvLastCheckedOutDate.text = context.getString(R.string.last_checked_out_date,
+                    "")
+        }
+    }
+}
