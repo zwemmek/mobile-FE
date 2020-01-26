25475100c8602cb5e66ddfbc2452c6e4ef8d7023
zwemmek <kevin.zwemmer@hva.nl>
2020-01-26 19:42:54 +0100

    login

--- app/src/main/java/com/example/checkerapp/model/LoggedInUser.kt
+++ app/src/main/java/com/example/checkerapp/model/LoggedInUser.kt
@@ -1,8 +1,6 @@
 package com.example.checkerapp.model
 
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
+
 data class LoggedInUser(
     val passId: String,
     val password: String
