0f46e5347d832519ed248ae0e1bf8aaeaa68570e
zwemmek <kevin.zwemmer@hva.nl>
2020-01-26 19:29:42 +0100

    login

--- app/src/main/java/com/example/checkerapp/api/LoginDataSource.kt
+++ app/src/main/java/com/example/checkerapp/api/LoginDataSource.kt
@@ -4,9 +4,7 @@ import com.example.checkerapp.model.LoggedInUser
 import com.example.checkerapp.repository.Result
 import java.io.IOException
 
-/**
- * Class that handles authentication w/ login credentials and retrieves user information.
- */
+
 class LoginDataSource {
 
     fun loginEmployee(passId: String, password: String): Result<LoggedInUser> {
