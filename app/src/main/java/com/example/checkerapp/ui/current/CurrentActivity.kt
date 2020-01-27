041b3f7fd7b761061ff39d65cb8b8470bc2674fb
Twan Rozie <i.am.thrownaway@gmail.com>
2020-01-27 09:51:04 +0100

    Viewmodel gevuld en currentActivity aangevuld zodat deze de actuele status kan binnenhalen.

--- app/src/main/java/com/example/checkerapp/ui/current/CurrentActivity.kt
+++ app/src/main/java/com/example/checkerapp/ui/current/CurrentActivity.kt
@@ -3,22 +3,47 @@ package com.example.checkerapp.ui.current
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.example.checkerapp.R
 import com.example.checkerapp.ui.change.ChangeActivity
+import com.example.checkerapp.ui.change.ChangeActivityViewModel
 import com.example.checkerapp.ui.history.HistoryActivity
+import kotlinx.android.synthetic.main.activity_change_status.*
+import kotlinx.android.synthetic.main.activity_current.*
 import kotlinx.android.synthetic.main.navigation_bar.*
 
 class CurrentActivity : AppCompatActivity() {
+
+    private lateinit var currentActivityViewModel: CurrentActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_current)
 
         initViews()
+        initViewModel()
     }
 
     private fun initViews() {
         navigate()
+    }
+
+    private fun initViewModel() {
+        currentActivityViewModel = ViewModelProviders.of(this).get(CurrentActivityViewModel::class.java)
+        currentActivityViewModel.getCurrentStatusByEmployeeId(66366)
+        currentActivityViewModel.statusHistory.observe(this, Observer { statusHistory ->
+            if (statusHistory != null) {
+                getEmployee(statusHistory.status)
+                tvActualStatusText.text = getString(R.string.checked_status, statusHistory.status)
+            }
+        })
+
+        currentActivityViewModel.error.observe(this, Observer {
+            Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+        })
+
     }
 
     private fun navigate(){
@@ -45,4 +70,23 @@ class CurrentActivity : AppCompatActivity() {
             Intent(this@CurrentActivity,
                 ChangeActivity::class.java)
         )} }
+
+    private fun getEmployee(statusHistory: String) {
+
+        when (statusHistory) {
+            "in" -> {
+                ivActualStatusPlaceHolder.setBackgroundResource(R.drawable.vinkje)
+                clActualStatus.setBackgroundColor(getResources().getColor(R.color.checked_in))
+                // and more color changes
+            }
+            "out" -> {
+                ivActualStatusPlaceHolder.setBackgroundResource(R.drawable.kruis)
+                clActualStatus.setBackgroundColor(getResources().getColor(R.color.checked_out))
+                // change colors to red-ish
+            }
+            else -> Toast.makeText(applicationContext,"Something went wrong \n guess you entered limbo...",
+                Toast.LENGTH_SHORT).show()
+        }
+
+    }
 }
--- app/src/main/java/com/example/checkerapp/ui/current/CurrentActivityViewModel.kt
+++ app/src/main/java/com/example/checkerapp/ui/current/CurrentActivityViewModel.kt
@@ -4,6 +4,39 @@ import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.checkerapp.model.ServerResponse
+import com.example.checkerapp.model.StatusHistory
+import com.example.checkerapp.repository.EmployeeApiRepository
+import com.google.gson.Gson
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class CurrentActivityViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val employeeApiRepository = EmployeeApiRepository()
+    val statusHistory = MutableLiveData<StatusHistory>()
+    val error = MutableLiveData<String>()
+    var responseSuccess = false
+
+    fun getCurrentStatusByEmployeeId(employeeId: Long) {
+        employeeApiRepository.getCurrentStatusByEmployeeId(employeeId).enqueue(object :
+            Callback<StatusHistory> {
+            override fun onResponse(call : Call<StatusHistory>, response: Response<StatusHistory>) {
+                if (response.isSuccessful) responseSuccess = true
+                if (response.isSuccessful) statusHistory.value = response.body()!!
+                else {
+                    val serverResponse = Gson().fromJson(response.errorBody()!!.string()
+                        , ServerResponse::class.java)
+                    error.value = serverResponse.message
+                }
+            }
+
+            override fun onFailure(call: Call<StatusHistory>, t: Throwable) {
+                error.value = t.message
+            }
+
+        })
+    }
 }
